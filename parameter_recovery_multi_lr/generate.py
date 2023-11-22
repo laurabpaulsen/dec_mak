9816b7aaaa9888b273548fe79a802ab3281522ae
laurabpaulsen <202005791@post.au.dk>
2023-11-22 12:46:04 +0100

    Add parameter recovery for serarate lr_r_sound and lr_r_shape

---
+++ parameter_recovery_multi_lr/generate.py
@@ -0,0 +1,236 @@
+"""
+This script holds the functions to generate the data for the parameter recovery. Additionally, when running this script as main, it will generate the data and figures of the associations and choices over trials.
+"""
+
+import pandas as pd
+import numpy as np
+import random
+import matplotlib.pyplot as plt
+from pathlib import Path
+
+def softmax(x, theta = 1):
+    """
+    Compute softmax values for each sets of scores in x
+
+    Parameters
+    ----------
+    x : numpy array
+        Array of values to apply softmax to
+    inverse_heat : float
+        Inverse temperature for the softmax. The default is 1.
+
+    Returns
+    -------
+    softmax_x : numpy array
+        Softmaxed values
+    """
+
+    # Joshs r code
+    # exp_p[t,] <- exp(theta*w[t,stim[t],])
+    # for (i in 1:nmel) {
+    #       p[t,i] <- exp_p[t,i]/sum(exp_p[t,])
+    # } 
+    
+    # get the exponentiated values
+    exp_x = np.exp(theta * x)
+
+    # get the softmaxed values
+    softmax_x = exp_x / np.sum(exp_x)
+
+    return softmax_x
+
+
+
+def associations_at_start(n_shapes = 5, n_sounds = 5):
+    """ 
+    Generates pandas dataframe with a uniform representation for the pairwise association between shapes and sounds
+    """
+
+    uniform_association = 1/n_shapes
+
+    # empty pandas dataframe one column for each shape and one row for each sound
+    df = pd.DataFrame(index = range(n_sounds), columns = range(n_shapes))
+
+    # fill dataframe with uniform association
+    df = df.fillna(uniform_association)
+
+    return df
+
+
+def update_associations(
+        sound_played:int, 
+        shape_chosen:int, 
+        feedback:bool, 
+        associations:pd.DataFrame, 
+        lr = 0.05, 
+        lr_r_sound = 0.05,  
+        lr_r__shape = 0.05, 
+        n_shapes = 5, 
+        n_sounds = 5):
+    """
+    Updates the associations between shapes and sounds based on the feedback received
+    
+    Parameters
+    ----------
+    sound_played : int
+        Index of the sound played
+    shape_chosen : int
+        Index of the shape chosen
+    feedback : bool
+        Feedback received for the choice made (True = correct, False = incorrect)
+    associations : pandas dataframe
+        Associations between shapes and sounds
+    
+    Returns
+    -------
+    None. The associations dataframe is updated in place
+    """
+
+    # update the chosen shape-sound association
+    if feedback: # if correct, increase the association
+        associations.loc[sound_played, shape_chosen] += lr
+    else: # if incorrect, decrease the association
+        associations.loc[sound_played, shape_chosen] -= lr
+    
+    # update the associations for shapes not chosen for the sound played
+    for i in range(n_shapes): 
+        if i != shape_chosen:
+            if feedback:
+                associations.loc[sound_played, i] -= lr_r_sound
+            else:
+                associations.loc[sound_played, i] += lr_r_sound
+
+    # update the associations for sounds not played for the shape chosen
+    for i in range(n_sounds):
+        if i != sound_played:
+            if feedback:
+                associations.loc[i, shape_chosen] -= lr_r__shape
+            else:
+                associations.loc[i, shape_chosen] +=  lr_r__shape
+
+def play_sound(n_sounds = 5):
+    """
+    Chooses a sound to play at random
+    """
+
+    return random.randint(0, n_sounds - 1)
+
+def choose_a_shape(associations:pd.DataFrame, sound_played:int, theta = 1):
+    """
+    Chooses a shape to play based on the associations between shapes and sounds
+    """
+
+    # get the associations for the sound played
+    sound_associations = associations.loc[sound_played, :]
+
+    # apply softmax to the associations
+    sound_associations_softmax = softmax(sound_associations, theta = theta)
+
+    # choose a shape based on the softmaxed associations
+    shape_chosen = np.random.choice(range(len(sound_associations)), p = sound_associations_softmax)
+
+    return shape_chosen
+
+def experimental_loop(n_trials, theta = 2, learning_rate = 0.05, reversal_learning_rate = 0.05, n_shapes = 5, n_sounds = 5):
+    """
+
+    Parameters
+    ----------
+    n_trials : int
+        Number of trials to run
+    sound_shape_mapping : dict
+        Dictionary mapping sounds to shapes where the keys are the sounds and the values are the shapess
+    theta : float
+        Inverse temperature for the softmax
+    learning_rate : float, optional
+        Learning rate for updating the associations. The default is 0.05.
+    n_shapes : int, optional
+        Number of shapes. The default is 5.
+    n_sounds : int, optional
+        Number of sounds. The default is 5.
+    
+    Returns
+    -------
+    associations_list : list
+        List of pandas dataframes with the associations between shapes and sounds at each trial
+    """
+    stimuli = np.zeros((n_trials))
+    response = np.zeros((n_trials))
+    hit = np.zeros((n_trials))
+
+    # get uniform associations at start
+    associations = associations_at_start()
+
+    # list to save associations at each trial
+    associations_list = [associations.copy()]
+
+
+    # loop over trials
+    for trial in range(n_trials):
+        sound = play_sound()
+        stimuli[trial] = sound
+
+        shape = choose_a_shape(associations, sound, theta = theta)
+
+        feedback = shape == sound
+
+        update_associations(
+            sound, 
+            shape, 
+            feedback, 
+            associations, 
+            learning_rate = learning_rate, 
+            reversal_learning_rate = reversal_learning_rate)
+
+        associations_list.append(associations.copy())
+
+        response[trial] = shape
+        hit[trial] = int(feedback)
+    
+    return associations_list, stimuli, response, hit
+
+
+def main():
+    path = Path(__file__).parent
+
+    fig_path = path / "fig"
+
+    # ensure that the fig folder exists
+    if not fig_path.exists():
+        fig_path.mkdir()
+
+    associations_list, response, hit = experimental_loop(
+        n_trials=100,
+        theta = 2,
+        learning_rate = 0.05, # learning rate for chosen nodes
+        reversal_learning_rate = 0.01 # learning rate for non-chosen nodes
+        )
+
+    # plot how the associations change over time for each sound
+    fig, axes = plt.subplots(1, 5, figsize = (20, 5), sharey=True)
+
+    for sound in range(5):
+        tmp_associations = [associations.loc[sound, :] for associations in associations_list]
+        axes[sound].plot(tmp_associations)
+        axes[sound].set_title(f"Sound {sound}")
+    
+    for ax in axes:
+        ax.set_xlabel("Trial")
+        ax.set_ylabel("Association")
+
+
+    plt.savefig(fig_path / "associations.png")
+
+    # plot the choices made over time and correct/incorrect feedback
+    fig, ax = plt.subplots(1, 1, figsize = (20, 5))
+
+    ax.plot(response, label = "Response")
+    ax.plot(hit, label = "Hit")
+    ax.legend()
+
+    ax.set_xlabel("Trial")
+    
+    plt.savefig(fig_path / "choices.png")
+
+if __name__ in "__main__":    
+    main()
---
+++ parameter_recovery_multi_lr/subject_parameter_recovery.py
@@ -0,0 +1,150 @@
+"""
+Loops over n_subjects and recovers the parameters for each subject.
+
+dev notes:
+- [ ] Figure out which distributions to use to draw the parameters from
+"""
+
+import stan
+from generate import experimental_loop
+from pathlib import Path
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+def plot_recovery_ax(ax, true, estimated, parameter_name):
+    """
+    Helper function for plot_recoveries
+    """
+    ax.scatter(true, estimated)
+    x_lims = ax.get_xlim()
+    ax.plot([0, x_lims[1]], [0, x_lims[1]], color = "black", linestyle = "dashed")
+    ax.set_xlabel("True")
+    ax.set_ylabel("Estimated")
+    ax.set_title(parameter_name.title())
+
+
+def plot_recoveries(trues:list, estimateds:list, parameter_names:list, savepath:Path):
+    """
+    Plot the recovery of the parameters.
+
+    Parameters
+    ----------
+    trues : list
+        List of true parameters.
+    estimateds : list
+        List of estimated parameters.
+    parameter_names : list
+        List of parameter names.
+    savepath : Path
+        Path to save the figure to.
+    
+    Returns
+    -------
+    None
+    """
+
+     # plot true vs estimated parameters
+    fig, axes = plt.subplots(1, len(trues), figsize = (15, 5))
+    
+    for true, estimated, parameter_name, axis in zip(trues, estimateds, parameter_names, axes):
+        plot_recovery_ax(axis, true, estimated, parameter_name)
+
+    plt.tight_layout()
+    
+    if savepath:
+        plt.savefig(savepath)
+
+
+def test_parameter_recovery(n_subjects, model_spec, savepath = None):
+    """
+    Generate synthetic data and fit the model to it. Check how well the parameters are recovered by plotting median against the true parameters.
+
+    Parameters
+    ----------
+    n_subjects : int
+        Number of subjects.
+    model_spec : str
+        Stan model specification.
+    savepath : Path, optional
+        Path to save the parameter recovery figure to, by default None
+    """
+    # true theta, learning rate, reversal learning rate
+    t_theta, t_lr, t_lr_r_sound, t_lr_r_shape = np.zeros((n_subjects)), np.zeros((n_subjects)), np.zeros((n_subjects)), np.zeros((n_subjects))
+
+    # estimated theta, learning rate, reversal learning rate
+    e_theta, e_lr, e_lr_r_sound, e_lr_r_shape = np.zeros((n_subjects)), np.zeros((n_subjects)), np.zeros((n_subjects)), np.zeros((n_subjects))
+
+    for subject in range(n_subjects):
+
+        # choose random parameters UPDATE THIS
+        theta = np.random.poisson(5)
+
+        # lr and rlr should be between 0 and 1 with more values closer to 0
+        lr = np.random.beta(1, 5)
+        lr_r_sound = np.random.beta(1, 5)
+        lr_r_shape = np.random.beta(1, 5)
+
+        # generate synthetic data
+        _, stimuli, response, hit = experimental_loop(
+            n_trials = 100, 
+            theta = theta,
+            lr = lr, # learning rate for chosen nodes
+            lr_r_sound = lr_r_sound, # learning rate for non-chosen nodes,
+            lr_r_shape = lr_r_shape
+        )
+
+        # prepare data for Stan
+        data = {
+            "n_trials": len(response),
+            "stim": [int(stim) + 1 for stim in stimuli], # stan starts counting at 1 instead of 0
+            "resp": [int(resp) + 1 for resp in response],
+            "hit": hit,
+            "sounds": list(range(1,6)),
+            "shapes": list(range(1,6)),
+        }
+
+        model = stan.build(model_spec, data=data)
+
+        fit = model.sample(num_chains=4, num_samples=1000)
+
+        df = fit.to_frame()  # pandas `DataFrame, requires pandas
+
+        # save true and e parameters
+        t_theta[subject] = theta
+        t_lr[subject] = lr
+        t_lr_r_sound[subject] = lr_r_sound
+        t_lr_r_shape[subject] = lr_r_shape
+
+        e_theta[subject] = df['theta'].median()
+        e_lr[subject] = df['lr'].median()
+        e_lr_r_sound[subject] = df['lr_r_sound'].median()
+        e_lr_r_shape[subject] = df['lr_r_shape'].median()
+    
+    # plot true vs estimated parameters
+    plot_recoveries(
+        trues = [t_theta, t_lr, t_lr_r_sound, t_lr_r_shape],
+        estimateds = [e_theta, e_lr, e_lr_r_sound, e_lr_r_shape],
+        parameter_names = ["theta", "lr", "reversal lr sound", "reversal lr shape"],
+        savepath = savepath
+    )
+   
+
+def main():
+    path = Path(__file__).parent
+
+    outpath = path / "fig"
+
+    if not outpath.exists():
+        outpath.mkdir()
+
+    with open(path.parent / "single_subject_multi_lr.stan") as f:
+        model_spec = f.read()
+    
+    n_subjects = 10
+
+    test_parameter_recovery(n_subjects, model_spec, savepath = outpath / "subject_parameter_recovery_sound_shape_lr.png")
+
+
+if __name__ in "__main__":
+    main()
